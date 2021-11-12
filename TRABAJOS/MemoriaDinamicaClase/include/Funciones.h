e9a21734f65b3a216ea928cf1b60b6322f53884f
arielp23 <eapozo4@espe.edu.ec>
2021-11-11 23:01:13 -0300

    11/11/2021

---
+++ TRABAJOS/MemoriaDinamicaClase/include/Funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+template <class H>
+void ingresar(H *, int);
+
+template <class H>
+void imprimir(H *, int);
+
+template <class H>
+void procesar(H *, int);
+
+#endif // FUNCIONES_H
