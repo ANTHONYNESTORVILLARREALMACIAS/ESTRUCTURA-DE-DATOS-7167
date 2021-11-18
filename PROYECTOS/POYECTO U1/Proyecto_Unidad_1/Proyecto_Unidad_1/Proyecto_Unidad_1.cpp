b6350f2c2376949ce7fde3798f875b87ec69d3ac
arielp21 <ariel.pf09@gmail.com>
2021-11-18 20:43:27 -0300

    18/11/2021

---
+++ PROYECTOS/POYECTO U1/Proyecto_Unidad_1/Proyecto_Unidad_1/Proyecto_Unidad_1.cpp
@@ -0,0 +1,22 @@
+/* UNIVERSIDAD DE LAS FUERZAS ARMADAS (ESPE)
+Nombre del programa: Proyecto Unidad 1
+Autores: Anthony Villarreal, Ariel Pozo, Esteban Chablay, Espinoza Luis
+Fecha de creacion: 18/11/2021
+Fecha de modificacion: 18/11/2021
+7167 ESTRUCTURA DE DATOS
+*/
+
+#include <iostream>
+#include "Cedula.h"
+#include "Cedula.cpp"
+#include "Fecha_de_nacimiento.h"
+#include "Fecha_de_nacimiento.cpp"
+#include "Persona.h"
+#include "Persona.cpp"
+#include "Vehiculos.h"
+#include "Vehiculos.cpp"
+
+int main()
+{
+    
+}
